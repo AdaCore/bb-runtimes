d4d0c1889a0df39bfe65a748993d97658ae81209
gingold <gingold@f8352e7e-cb20-0410-8ce7-b5d9e71c585c>
2016-10-07 13:57:39 +0000

    More aarch64 commands.
    TN: P922-019
    
    
    git-svn-id: svn+ssh://svn.us.adacore.com/Dev/trunk/bb-runtimes@324585 f8352e7e-cb20-0410-8ce7-b5d9e71c585c

--- examples/monitor/aarch64/aarch64.adb
+++ examples/monitor/aarch64/aarch64.adb
@@ -44,6 +44,26 @@ package body Aarch64 is
       return Res;
    end Get_Current_EL;
 
+   function Get_DAIF return Unsigned_32
+   is
+      Res : Unsigned_32;
+   begin
+      Asm ("mrs %0, daif",
+           Outputs => Unsigned_32'Asm_Output ("=r", Res),
+           Volatile => True);
+      return Res;
+   end Get_DAIF;
+
+   function Get_SPSel return Unsigned_32
+   is
+      Res : Unsigned_32;
+   begin
+      Asm ("mrs %0, spsel",
+           Outputs => Unsigned_32'Asm_Output ("=r", Res),
+           Volatile => True);
+      return Res;
+   end Get_SPSel;
+
    function Get_ELR_EL3 return Unsigned_64
    is
       Res : Unsigned_64;
@@ -116,6 +136,11 @@ package body Aarch64 is
 
    procedure Proc_El1 is
    begin
+      if Get_Current_EL /= 3 * 4 then
+         Put_Line ("Not in EL3!");
+         return;
+      end if;
+
       Asm ("adr x0, 1f" & ASCII.LF & ASCII.HT &
            "msr elr_el3, x0" & ASCII.LF & ASCII.HT &
            "mov x0, #0x1c5"  & ASCII.LF & ASCII.HT &
@@ -129,6 +154,44 @@ package body Aarch64 is
            Clobber => "x0");
    end Proc_El1;
 
+   procedure Proc_El2 is
+   begin
+      if Get_Current_EL /= 3 * 4 then
+         Put_Line ("Not in EL3!");
+         return;
+      end if;
+
+      Asm ("adr x0, 1f" & ASCII.LF & ASCII.HT &
+           "msr elr_el3, x0" & ASCII.LF & ASCII.HT &
+           "mov x0, #0x1c9"  & ASCII.LF & ASCII.HT &
+           "msr spsr_el3, x0"  & ASCII.LF & ASCII.HT &
+           "mov x0, sp"  & ASCII.LF & ASCII.HT &
+           "msr sp_el2, x0"   & ASCII.LF & ASCII.HT &
+           "isb"  & ASCII.LF & ASCII.HT &
+           "eret"  & ASCII.LF & ASCII.HT &
+           "1:",
+           Volatile => True,
+           Clobber => "x0");
+   end Proc_El2;
+
+   procedure Proc_Svc is
+   begin
+      Asm ("svc #0",
+           Volatile => True);
+   end Proc_Svc;
+
+   procedure Proc_Hvc is
+   begin
+      Asm ("hvc #0",
+           Volatile => True);
+   end Proc_Hvc;
+
+   procedure Proc_Smc is
+   begin
+      Asm ("smc #0",
+           Volatile => True);
+   end Proc_Smc;
+
    procedure Proc_Cr
    is
       L : Natural;
@@ -140,8 +203,15 @@ package body Aarch64 is
          Put (Hex4 (EL));
          Put (" (");
          Put (Hex1 ((EL / 4) and 3));
-         Put_Line (")");
+         Put (")");
          L := Natural ((EL / 4) and 3);
+
+         Put (", DAIF: ");
+         Put (Hex4 (Get_DAIF));
+
+         Put (", SPSEL: ");
+         Put (Hex4 (Get_SPSel));
+         New_Line;
       end;
 
       if L >= 3 then
@@ -155,17 +225,15 @@ package body Aarch64 is
          Put ("EL3 SCR: ");
          Put (Hex8 (Get_SCR_EL3));
          New_Line;
-      end if;
-
-      if L >= 2 then
          Put ("EL2 SP: ");
          Put (Hex8 (Get_SP_EL2));
-         Put (", HCR: ");
-         Put (Hex8 (Get_HCR_EL2));
          New_Line;
       end if;
 
-      if L > 1 then
+      if L >= 2 then
+         Put ("EL2 HCR: ");
+         Put (Hex8 (Get_HCR_EL2));
+         New_Line;
          Put ("EL1 SP: ");
          Put (Hex8 (Get_SP_EL1));
          New_Line;
@@ -173,11 +241,19 @@ package body Aarch64 is
    end Proc_Cr;
 
    Commands : aliased Command_List :=
-     (2,
+     (6,
       (1 => (new String'("cr - Display some config registers"),
              Proc_Cr'Access),
        2 => (new String'("el1 - Switch to el1"),
-             Proc_El1'Access)),
+             Proc_El1'Access),
+       3 => (new String'("el2 - Switch to el2"),
+             Proc_El2'Access),
+       4 => (new String'("svc - sys call"),
+             Proc_Svc'Access),
+       5 => (new String'("hvc - hyper call"),
+             Proc_Hvc'Access),
+       6 => (new String'("smc - monitor call"),
+             Proc_Smc'Access)),
       null);
 begin
    Register_Commands (Commands'Access);
