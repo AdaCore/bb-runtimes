22033c1c11e5a941c57c4be8a6d70ec2f9e9a690
gingold <gingold@f8352e7e-cb20-0410-8ce7-b5d9e71c585c>
2017-06-14 14:39:30 +0000

    Remove unused dependency.
    TN: Q613-006
    
    
    git-svn-id: svn+ssh://svn.us.adacore.com/Dev/trunk/bb-runtimes@328698 f8352e7e-cb20-0410-8ce7-b5d9e71c585c

--- src/s-bbthre.ads
+++ src/s-bbthre.ads
@@ -8,7 +8,7 @@
 --                                                                          --
 --        Copyright (C) 1999-2002 Universidad Politecnica de Madrid         --
 --             Copyright (C) 2003-2005 The European Space Agency            --
---                     Copyright (C) 2003-2016, AdaCore                     --
+--                     Copyright (C) 2003-2017, AdaCore                     --
 --                                                                          --
 -- GNARL is free software; you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -42,7 +42,6 @@ with System;
 with System.Storage_Elements;
 with System.BB.CPU_Primitives;
 with System.BB.Time;
-with System.BB.Interrupts;
 with System.BB.Board_Support;
 with System.Multiprocessors;
 
