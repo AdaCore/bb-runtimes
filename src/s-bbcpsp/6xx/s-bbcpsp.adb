cb7b287c7e3d81fd596af0d94c5a7d911e56fdad
gingold <gingold@f8352e7e-cb20-0410-8ce7-b5d9e71c585c>
2017-06-15 07:53:23 +0000

    Remove dependencies on unreferenced units.
    TN: Q613-006
    
    
    
    git-svn-id: svn+ssh://svn.us.adacore.com/Dev/trunk/bb-runtimes@328724 f8352e7e-cb20-0410-8ce7-b5d9e71c585c

--- src/s-bbcpsp/6xx/s-bbcpsp.adb
+++ src/s-bbcpsp/6xx/s-bbcpsp.adb
@@ -8,7 +8,7 @@
 --                                                                          --
 --        Copyright (C) 1999-2002 Universidad Politecnica de Madrid         --
 --             Copyright (C) 2003-2005 The European Space Agency            --
---                     Copyright (C) 2003-2016, AdaCore                     --
+--                     Copyright (C) 2003-2017, AdaCore                     --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -32,8 +32,6 @@
 
 --  This package implements PowerPC architecture specific support for the GNAT
 --  Ravenscar run time.
-
-with System.BB.Interrupts;
 
 package body System.BB.CPU_Specific is
 
