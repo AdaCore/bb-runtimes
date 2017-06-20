24a08ec1cc94181077d356bc0719e07cc27befdc
gingold <gingold@f8352e7e-cb20-0410-8ce7-b5d9e71c585c>
2017-06-20 10:09:50 +0000

    aarch64: add a memory dumper in trap_dump
    TN: Q511-014
    
    
    git-svn-id: svn+ssh://svn.us.adacore.com/Dev/trunk/bb-runtimes@328812 f8352e7e-cb20-0410-8ce7-b5d9e71c585c

--- src/aarch64/trap_dump.adb
+++ src/aarch64/trap_dump.adb
@@ -38,13 +38,32 @@ package body Trap_Dump is
    procedure Put (Item : String);
    procedure New_Line;
    procedure Get (C : out Character);
+   procedure Put_Line (Item : String);
 
    procedure Put_Hex8 (V : Unsigned_64);
    procedure Put_Hex4 (V : Unsigned_32);
    procedure Put_Hex1 (V : Unsigned_8);
    procedure Put_02 (N : Natural);
 
+   subtype Line_Range is Natural range 1 .. 32;
+   Line : String (Line_Range);
+   Line_Len : Natural range 0 .. Line_Range'Last;
+
    Hex_Digits : constant array (0 .. 15) of Character := "0123456789abcdef";
+
+   subtype Hex_Digit_Type is Integer range -1 .. 255;
+   Bad_Hex : constant Hex_Digit_Type := -1;
+
+   function Read_Hex_Digit (Pos : Line_Range) return Hex_Digit_Type;
+   --  Convert Line (Pos) to its hexadecimal value.  Return Bad_Hex if not
+   --  an hexa character.
+
+   procedure Get_Line;
+   --  Set Line, Line_Len from input.
+
+   function Get_Unsigned_32 return Unsigned_32;
+
+   procedure Mem_Dump32;
 
    procedure Put (Item : Character) is
    begin
@@ -70,6 +89,12 @@ package body Trap_Dump is
 
       Put (ASCII.LF);
    end New_Line;
+
+   procedure Put_Line (Item : String) is
+   begin
+      Put (Item);
+      New_Line;
+   end Put_Line;
 
    procedure Put_Hex8 (V : Unsigned_64) is
       Res : String (1 .. 16);
@@ -137,6 +162,152 @@ package body Trap_Dump is
       Put_Hex1 (Unsigned_8 (Shift_Right (SPSR, 2)) and 3);
       Put (')');
    end Print_SPSR;
+
+   function Read_Hex_Digit (Pos : Line_Range) return Hex_Digit_Type is
+      C : constant Character := Line (Pos);
+   begin
+      case C is
+         when '0' .. '9' =>
+            return Character'Pos (C) - Character'Pos ('0');
+         when 'a' .. 'f' =>
+            return Character'Pos (C) - Character'Pos ('a') + 10;
+         when 'A' .. 'F' =>
+            return Character'Pos (C) - Character'Pos ('A') + 10;
+         when others =>
+            return Bad_Hex;
+      end case;
+   end Read_Hex_Digit;
+
+   procedure Get_Line is
+      C : Character;
+      Prev_Len : Natural := Line_Len;
+   begin
+      Put ("> ");
+      Line_Len := 0;
+      loop
+         Get (C);
+         case C is
+            when ' ' .. Character'Val (126) =>
+               if Line_Len < Line_Range'Last then
+                  Line_Len := Line_Len + 1;
+                  Line (Line_Len) := C;
+                  Put (C);
+               else
+                  Put (ASCII.BEL);
+               end if;
+            when ASCII.CR | ASCII.LF =>
+               New_Line;
+               exit;
+            when Character'Val (21) => -- ^U
+               New_Line;
+               Put ("> ");
+               Line_Len := 0;
+            when Character'Val (8)  --  ^H
+              | Character'Val (127) => -- DEL
+               if Line_Len = 0 then
+                  Put (ASCII.BEL);
+               else
+                  Put (ASCII.BS);
+                  Put (' ');
+                  Put (ASCII.BS);
+                  Line_Len := Line_Len - 1;
+                  Prev_Len := 0;
+               end if;
+            when Character'Val (16) => -- ^P
+               if Line_Len = 0 and then Prev_Len > 0 then
+                  --  Recall previous line
+                  for I in 1 .. Prev_Len loop
+                     Put (Line (I));
+                  end loop;
+                  Line_Len := Prev_Len;
+               else
+                  Put (ASCII.BEL);
+               end if;
+            when others =>
+               Put (ASCII.BEL);
+         end case;
+      end loop;
+   end Get_Line;
+
+   function Get_Unsigned_32 return Unsigned_32
+   is
+      H : Hex_Digit_Type;
+      C : Character;
+      Pos : Line_Range;
+      Res : Unsigned_32;
+   begin
+      <<Again>> null;
+      Get_Line;
+      Pos := Line'First;
+      Res := 0;
+
+      if Line_Len > Pos + 1
+        and then Line (Pos) = '0'
+        and then Line (Pos + 1) = 'x'
+      then
+         --  Parse as hex.
+         for I in Pos + 2 .. Line_Len loop
+            H := Read_Hex_Digit (I);
+            if H = Bad_Hex then
+               Put_Line ("Bad character in hex number");
+               goto Again;
+            else
+               Res := Res * 16 + Unsigned_32 (H);
+            end if;
+         end loop;
+      else
+         --  Parse as dec.
+         for I in Pos .. Line_Len loop
+            Res := Res * 10;
+            C := Line (I);
+            if C in '0' .. '9' then
+               Res := Res + Character'Pos (C) - Character'Pos ('0');
+            else
+               Put_Line ("Bad character in decimal number");
+               goto Again;
+            end if;
+         end loop;
+      end if;
+      return Res;
+   end Get_Unsigned_32;
+
+   procedure Mem_Dump32 is
+      Addr : Unsigned_32;
+      Len : Unsigned_32;
+      Eaddr : Unsigned_32;
+   begin
+      Put ("address");
+      Addr := Get_Unsigned_32;
+      Put ("length");
+      Len := Get_Unsigned_32;
+
+      --  Align address
+      Addr := Addr and not 3;
+
+      Eaddr := Addr + Len - 1;
+      loop
+         Put_Hex4 (Addr);
+         Put (": ");
+         for I in Unsigned_32 range 0 .. 3 loop
+            if Addr > Eaddr then
+               Put ("        ");
+            else
+               declare
+                  W : Unsigned_32;
+                  for W'Address use System'To_Address (Addr);
+                  pragma Import (Ada, W);
+               begin
+                  Put_Hex4 (W);
+               end;
+            end if;
+            Put (' ');
+            Addr := Addr + 4;
+         end loop;
+
+         New_Line;
+         exit when Addr - 1 >= Eaddr; --  Handle wrap around 0
+      end loop;
+   end Mem_Dump32;
 
    procedure Dump (Regs : Registers_List_Acc; Id : Natural)
    is
@@ -245,6 +416,11 @@ package body Trap_Dump is
          Put (", TCR:");
          Put_Hex8 (Get_TCR_EL1);
          New_Line;
+         Put ("EL1 TTBR0:");
+         Put_Hex8 (Get_TTBR0_EL1);
+         Put (", TTBR1:");
+         Put_Hex8 (Get_TTBR1_EL1);
+         New_Line;
       end if;
 
       --  Interractive (!!) session.
@@ -271,6 +447,9 @@ package body Trap_Dump is
                exit;
             when 'r' | 'R' =>
                System.Machine_Reset.Stop;
+            when 'd' =>
+               New_Line;
+               Mem_Dump32;
             when others =>
                Put ('?');
          end case;
