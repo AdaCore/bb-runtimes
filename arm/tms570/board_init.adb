bf31723de0d25d8804320c94c1a82225827ad954
Jerome Lambourg <lambourg@adacore.com>
2019-02-27 10:17:14 +0100

    tms470: fix peripheral region 2 address when setting the MPU.
    Change-Id: I5f067bbc520ecb846a87901f9e0445118ffe057d
    TN: S226-039

--- arm/tms570/board_init.adb
+++ arm/tms570/board_init.adb
@@ -2,7 +2,7 @@
 --                                                                          --
 --                         GNAT RUN-TIME COMPONENTS                         --
 --                                                                          --
---                      Copyright (C) 2016-2017, AdaCore                    --
+--                      Copyright (C) 2016-2019, AdaCore                    --
 --                                                                          --
 -- GNAT is free software;  you can  redistribute it  and/or modify it under --
 -- terms of the  GNU General Public License as published  by the Free Soft- --
@@ -240,7 +240,7 @@ package body Board_Init is
 
       --  Region 7: 16MB peripheral segment 2
       CP15.Set_MPU_Region_Number (6);
-      CP15.Set_MPU_Region_Base_Address    (16#F000_0000#);
+      CP15.Set_MPU_Region_Base_Address    (16#FC00_0000#);
       CP15.Set_MPU_Region_Size_And_Enable (Size_16MB or 1);
       CP15.Set_MPU_Region_Access_Control  (XN or AP_RW_RW or NON_SHAREABLE);
 
