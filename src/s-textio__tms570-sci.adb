7f261e7c7ead2bd17ef3c4f237c44c9e0330fe08
Jerome Lambourg <lambourg@adacore.com>
2019-11-13 15:06:02 +0100

    Add doc for Interfaces.ARM_v7AR and TMS570's System.Text_IO.
    Change-Id: I568af9c4a1e12049449ece5a77ab22b67c0e2069
    TN: S711-016

--- src/s-textio__tms570-sci.adb
+++ src/s-textio__tms570-sci.adb
@@ -32,8 +32,19 @@
 --  Minimal version of Text_IO body for use on TMS570 family of MCUs, using
 --  SCI1/LIN1
 
+--  @design
+--  This package is in charge of sending characters to the remote host
+--  machine. The application output is sent through the UART, from which the
+--  host machine extracts the application output.
+--
+--  The TMS570 runtime uses the SCI1/LIN1 module, configured for 115200 baud
+--  rate, one stop bit, no parity.
+
 with Interfaces;              use Interfaces;
+--  @design used for the 32-bit integers definition
 with System.Board_Parameters; use System.Board_Parameters;
+--  @design used to retrieve the system clock, used to calculate the prescaler
+--  values to achieve the desired baud rate.
 
 package body System.Text_IO is
 
