3e84b3fd1763f3d260e177e7abc26194d26c53d1
Daniel King <dmking@adacore.com>
2023-10-27 16:46:27 +0100

    morello: Add instruction barriers after important system register writes
    This patch prevents instructions from being reordered which could result
    in an attempt to use processor features before they are properly enabled.
    
    Issue: eng/toolchain/bb-runtimes#15

--- src/s-bbcppr__morello.adb
+++ src/s-bbcppr__morello.adb
@@ -312,6 +312,7 @@ package body System.BB.CPU_Primitives is
       V := Get_CPACR_EL1;
       V := V and not CPACR_FPEN;
       Set_CPACR_EL1 (V);
+      ISB;
    end Disable_FPU;
 
    ----------------
@@ -324,6 +325,7 @@ package body System.BB.CPU_Primitives is
       V := Get_CPACR_EL1;
       V := V or CPACR_FPEN;
       Set_CPACR_EL1 (V);
+      ISB;
    end Enable_FPU;
 
    ---------------------
